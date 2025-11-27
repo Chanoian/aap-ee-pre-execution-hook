a525be776c238858d5102c4f05d4ad2367a197e9
Ara Chanoian <shanoian90@gmail.com>
2025-11-27 15:54:35 -0500

    init commit

--- precheck.sh
+++ precheck.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "Precheck Script"
 
+ls -l /runner/env >&2 || echo "/runner/env not present yet" >&2
+
 /usr/bin/python3.11 /usr/local/bin/validation/precheck.py
 
 echo "Precheck completed successfully"
